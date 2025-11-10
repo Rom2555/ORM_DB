05063ded566cae99e18959a85cdaec9d0f10a6ad
Roman E <rk2555@yandex.ru>
2025-11-10 16:16:58 +0300

    названия классов поправлены

--- model.py
+++ model.py
@@ -1,28 +1,28 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_base
 
-
 Base = declarative_base()
 
-class publisher(Base):
+
+class Publisher(Base):
     __tablename__ = 'publisher'
     id = sa.Column(sa.Integer, primary_key=True)
 
 
-class book(Base):
+class Book(Base):
     __tablename__ = 'book'
     id = sa.Column(sa.Integer, primary_key=True)
     title = sa.Column(sa.String(255))
     id_publisher = sa.Column(sa.Integer, sa.ForeignKey('publisher.id'))
 
 
-class shop(Base):
+class Shop(Base):
     __tablename__ = 'shop'
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String(255))
 
 
-class sale(Base):
+class Sale(Base):
     __tablename__ = 'sale'
     id = sa.Column(sa.Integer, primary_key=True)
     price = sa.Column(sa.Float)
@@ -31,7 +31,7 @@ class sale(Base):
     count = sa.Column(sa.Integer)
 
 
-class stock(Base):
+class Stock(Base):
     id = sa.Column(sa.Integer, primary_key=True)
     id_book = sa.Column(sa.Integer, sa.ForeignKey('book.id'))
     id_shop = sa.Column(sa.Integer, sa.ForeignKey('shop.id'))
