9ce1b59ed0939fc9a4b71cb3593f420a045a28cf
Roman E <rk2555@yandex.ru>
2025-11-10 16:49:58 +0300

    изменены связи

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import sqlalchemy as sa
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -7,21 +7,34 @@ Base = declarative_base()
 class Publisher(Base):
     __tablename__ = 'publisher'
     id = sa.Column(sa.Integer, primary_key=True)
-    books = sa.orm.relationship('Book', back_populates='publisher')
+    books = relationship('Book', back_populates='publisher')
+
 
 class Book(Base):
     __tablename__ = 'book'
     id = sa.Column(sa.Integer, primary_key=True)
     title = sa.Column(sa.String(255))
     id_publisher = sa.Column(sa.Integer, sa.ForeignKey('publisher.id'))
-    publisher = sa.orm.relationship('Publisher', back_populates='books')
-    stocks = sa.orm.relationship('Stock', back_populates='books')
+    publisher = relationship('Publisher', back_populates='books')
+    stocks = relationship('Stock', back_populates='book')  # ← один Book → много Stock
+
 
 class Shop(Base):
     __tablename__ = 'shop'
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String(255))
-    stocks = sa.orm.relationship('Stock', back_populates='shop')
+    stocks = relationship('Stock', back_populates='shop')  # ← один Shop → много Stock
+
+
+class Stock(Base):
+    __tablename__ = 'stock'
+    id = sa.Column(sa.Integer, primary_key=True)
+    id_book = sa.Column(sa.Integer, sa.ForeignKey('book.id'))
+    id_shop = sa.Column(sa.Integer, sa.ForeignKey('shop.id'))
+    count = sa.Column(sa.Integer)
+    book = relationship('Book', back_populates='stocks')  # ← один Book
+    shop = relationship('Shop', back_populates='stocks')  # ← один Shop
+    sales = relationship('Sale', back_populates='stock')  # ← один Stock → много Sale
 
 
 class Sale(Base):
@@ -31,14 +44,4 @@ class Sale(Base):
     date_sale = sa.Column(sa.Date)
     id_stock = sa.Column(sa.Integer, sa.ForeignKey('stock.id'))
     count = sa.Column(sa.Integer)
-    stocks = sa.orm.relationship('Stock', back_populates='sales')
-
-
-class Stock(Base):
-    id = sa.Column(sa.Integer, primary_key=True)
-    id_book = sa.Column(sa.Integer, sa.ForeignKey('book.id'))
-    id_shop = sa.Column(sa.Integer, sa.ForeignKey('shop.id'))
-    count = sa.Column(sa.Integer)
-    shop = sa.orm.relationship('Shop', back_populates='stocks')
-    sales = sa.orm.relationship('Sale', back_populates='stocks')
-    books = sa.orm.relationship('Book', back_populates='stocks')
+    stock = relationship('Stock', back_populates='sales')  # ← один Stock
