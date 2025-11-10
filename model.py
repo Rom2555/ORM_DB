6cdcd7f746ca40c71255735ff5640ae391747880
Roman E <rk2555@yandex.ru>
2025-11-10 14:08:20 +0300

    написаны классы по схеме

--- model.py
+++ model.py
@@ -16,3 +16,23 @@ class book(Base):
     id_publisher = sa.Column(sa.Integer, sa.ForeignKey('publisher.id'))
 
 
+class shop(Base):
+    __tablename__ = 'shop'
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(255))
+
+
+class sale(Base):
+    __tablename__ = 'sale'
+    id = sa.Column(sa.Integer, primary_key=True)
+    price = sa.Column(sa.Float)
+    date_sale = sa.Column(sa.Date)
+    id_stock = sa.Column(sa.Integer, sa.ForeignKey('stock.id'))
+    count = sa.Column(sa.Integer)
+
+
+class stock(Base):
+    id = sa.Column(sa.Integer, primary_key=True)
+    id_book = sa.Column(sa.Integer, sa.ForeignKey('book.id'))
+    id_shop = sa.Column(sa.Integer, sa.ForeignKey('shop.id'))
+    count = sa.Column(sa.Integer)
