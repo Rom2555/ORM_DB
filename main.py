22437bdd110dd0f595318c6d1cc2d67cc39fc7cc
Roman E <rk2555@yandex.ru>
2025-11-11 08:11:46 +0300

    DSN, engine, session

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import Publisher, Book, Stock, Shop, Sale
+from models import Publisher, Book, Stock, Shop, Sale, Base
 
+
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+DSN = "postgresql://postgres:postgres@localhost:5432/orm_db"
+
+engine = sqlalchemy.create_engine(DSN)
+
+
+create_tables(engine)
+
+# сессия
+Session = sessionmaker(bind=engine)
+session = Session()
